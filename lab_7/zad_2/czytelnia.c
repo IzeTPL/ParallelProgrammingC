c8a5d6a551d6d8c6913d0fa829c627bfe85290eb
IzeTPL <marcin122131@gmail.com>
2016-11-22 01:13:01 +0100

    zad_2 fixed

--- lab_7/zad_2/czytelnia.c
+++ lab_7/zad_2/czytelnia.c
@@ -10,8 +10,10 @@
 
 int my_read_lock_lock(czytelnia_t *czytelnia_p) {
     pthread_mutex_lock(&czytelnia_p->mutex);
-    if(czytelnia_p->liczba_pisz > 0 || !czytelnia_p->pisarze) {
+    if(czytelnia_p->liczba_pisz > 0 || czytelnia_p->liczba_czek_pisz != 0) {
+        czytelnia_p->liczba_czek_czyt++;
         pthread_cond_wait(&czytelnia_p->czytelnicy, &czytelnia_p->mutex);
+        czytelnia_p->liczba_czek_czyt--;
     }
     czytelnia_p->liczba_czyt++;
     pthread_mutex_unlock(&czytelnia_p->mutex);
@@ -34,7 +36,9 @@ int my_read_lock_unlock(czytelnia_t *czytelnia_p) {
 int my_write_lock_lock(czytelnia_t *czytelnia_p) {
     pthread_mutex_lock(&czytelnia_p->mutex);
     if(czytelnia_p->liczba_czyt+czytelnia_p->liczba_pisz > 0) {
+        czytelnia_p->liczba_czek_pisz++;
         pthread_cond_wait(&czytelnia_p->pisarze, &czytelnia_p->mutex);
+        czytelnia_p->liczba_czek_pisz++;
     }
     czytelnia_p->liczba_pisz++;
     pthread_mutex_unlock(&czytelnia_p->mutex);
@@ -44,7 +48,7 @@ int my_write_lock_lock(czytelnia_t *czytelnia_p) {
 int my_write_lock_unlock(czytelnia_t *czytelnia_p) {
     pthread_mutex_lock(&czytelnia_p->mutex);
     czytelnia_p->liczba_pisz--;
-    if(czytelnia_p->czytelnicy) {
+    if(czytelnia_p->liczba_czek_czyt != 0) {
         pthread_mutex_unlock(&czytelnia_p->mutex);
         pthread_cond_broadcast(&czytelnia_p->czytelnicy);
     } else {
@@ -56,6 +60,8 @@ int my_write_lock_unlock(czytelnia_t *czytelnia_p) {
 void inicjuj(czytelnia_t *czytelnia_p) {
     czytelnia_p->liczba_pisz = 0;
     czytelnia_p->liczba_czyt = 0;
+    czytelnia_p->liczba_czek_pisz = 0;
+    czytelnia_p->liczba_czek_czyt = 0;
     czytelnia_p->czytelnicy = PTHREAD_COND_INITIALIZER;
     czytelnia_p->pisarze = PTHREAD_COND_INITIALIZER;
     czytelnia_p->mutex = PTHREAD_MUTEX_INITIALIZER;
